981235829b0ccee8d3b2f270fe5aa0320b68ed3e
Wilson Maximo <maximortywil@gmail.com>
2023-03-16 10:23:51 -0300

    Questão 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table>
         { console.log(this.props) }
@@ -19,10 +21,45 @@ class Table extends Component {
             <th>Editar/Excluir</th>
           </tr>
         </thead>
+        <tbody>
+          { expenses?.map((expense, index) => (
+            <tr key={ index }>
+              <td>
+                { expense.description }
+              </td>
+              <td>
+                { expense.tag }
+              </td>
+              <td>
+                { expense.method }
+              </td>
+              <td>
+                { Number(expense.value).toFixed(2) }
+              </td>
+              <td>
+                { expense.currency }
+              </td>
+              <td>
+                { Number(expense.exchangeRates[expense.currency].ask).toFixed(2) }
+              </td>
+              <td>
+                { (expense.value
+                  * expense.exchangeRates[expense.currency].ask).toFixed(2) }
+              </td>
+              <td>
+                { expense.exchangeRates[expense.currency].name }
+              </td>
+            </tr>
+          )) }
+        </tbody>
       </table>
     );
   }
 }
+
+Table.propTypes = {
+  expenses: PropTypes.instanceOf(Array).isRequired,
+};
 
 const mapStateToProps = (state) => ({
   ...state.wallet,
